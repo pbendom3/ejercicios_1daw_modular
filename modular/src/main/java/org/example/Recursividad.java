24887c31941486978b012d7cff6bafb7b672f29a
A600\Patricia <p.benaventedomenec@edu.gva.es>
2026-01-09 17:20:26 +0100

    ++

--- modular/src/main/java/org/example/Recursividad.java
+++ modular/src/main/java/org/example/Recursividad.java
@@ -3,11 +3,52 @@ package org.example;
 public class Recursividad {
 
     static int limite = 10;
+    static String frase = "Hola soy una frase al reves";
+    static char[] frase_char = frase.toCharArray();
 
     static void main() {
 
+        System.gc(); //Limpia lo que pueda antes de medir
+
+        long inicioEjecucion = System.nanoTime();
+        long memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        // PROGRAMA A REALIZAR
         imprimir(1);
-        System.out.println(suma(10));
+
+        long finEjecucion = System.nanoTime();
+        long memoriaDespues = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.println("Tiempo recursivo: " + (finEjecucion - inicioEjecucion) + " ns");
+        System.out.println("Memoria consumida recursivo: " + (memoriaDespues - memoriaAntes) + " bytes");
+
+        inicioEjecucion = System.nanoTime();
+        memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        // PROGRAMA A REALIZAR
+        imprimir_bucle();
+
+        finEjecucion = System.nanoTime();
+        memoriaDespues = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.println("Tiempo: " + (finEjecucion - inicioEjecucion) + " ns");
+        System.out.println("Memoria consumida: " + (memoriaDespues - memoriaAntes) + " bytes");
+
+     //   System.out.println(suma(10));
+
+        System.out.println(digitos(537473));
+
+        System.out.println(potencia(2,4));
+
+        del_reves(38472);
+
+        System.out.println();
+
+        del_reves_char(frase_char.length-1);
+
+        System.out.println();
+
+        del_reves_char_sin_vector(frase.length()-1);
 
     }
 
@@ -16,6 +57,14 @@ public class Recursividad {
         if (num <= limite){
             System.out.println(num);
             imprimir(num+1);
+        }
+
+    }
+
+    public static void imprimir_bucle(){
+
+        for (int i = 1; i <= 10000; i++) {
+            System.out.println(i);
         }
 
     }
@@ -29,4 +78,54 @@ public class Recursividad {
         }
 
     }
+
+    public static int digitos (int numero){
+
+        if (numero < 10){
+            return 1;
+        }else{
+            return 1+digitos(numero/10);
+        }
+
+    }
+
+    public static int potencia (int base, int exponente){
+
+        if (exponente == 0){
+            return 1;
+        }else{
+            return base*potencia(base,exponente-1);
+        }
+
+    }
+
+    public static void del_reves(int numero){
+
+        if (numero < 10){
+            System.out.print(numero);
+        }else{
+            System.out.print(numero%10);
+            del_reves(numero/10);
+        }
+
+    }
+
+    public static void del_reves_char(int posicion){
+
+        if (posicion>=0){
+            System.out.print(frase_char[posicion]);
+            del_reves_char(posicion-1);
+        }
+
+    }
+
+    public static void del_reves_char_sin_vector(int posicion){
+
+        if (posicion>=0){
+            System.out.print(frase.charAt(posicion));
+            del_reves_char(posicion-1);
+        }
+
+    }
+
 }
